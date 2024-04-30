d04895e0c3f3237290078270e7aea46714507394
dbsdndcks <ywcsuwon@naver.com>
2024-04-30 17:57:46 +0900

    day0430 include 추가

--- src/main/java/VO/MovieVO.java
+++ src/main/java/VO/MovieVO.java
@@ -1,7 +1,27 @@
 package VO;
 
-public class MovieVO {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-	
-	
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MovieVO {
+	private String movieCode;
+	private String movieTitle;
+	private String movieReleaseDate;
+	private String movieGenre;
+	private String movieRating;
+	private String movieDirector;
+	private String movieDistributor;
+	private String movieRunningTime;
+	private String movieMainActor;
+	private String movieSubActor;
+	private String movieDescription;
+	private String moviePosterPath;
+	private String movieScreeningStatus;
+	private String movieInputDate;
 }
