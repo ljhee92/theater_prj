e3dd2d7b2a49e7cbac7d4dc68d40e37cc71e1600
YeongKing <whdcks208@naver.com>
2024-05-10 19:49:27 +0900

    마이페이지 나의예매내역 구현 및 관련 DAO VO 추가

---
+++ src/main/java/admin/servlet/SelectReservationServlet.java
@@ -0,0 +1,78 @@
+package admin.servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import user.DAO.UserReservationDAO;
+import user.VO.UserReservationVO;
+
+@WebServlet("/SelectReservationServlet")
+public class SelectReservationServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        System.out.println("서블릿 입장");
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json;charset=UTF-8");
+        String userId = request.getParameter("id");
+
+        try {
+            String resultJSON = getJSON(userId);
+            response.getWriter().write(resultJSON);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            response.getWriter().write("{\"success\":false}");
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.getWriter().write("{\"success\":false}");
+        }
+
+    }
+
+    public String getJSON(String userId) throws SQLException {
+        StringBuilder result = new StringBuilder("");
+        result.append("{\"success\":");
+        UserReservationDAO urDAO = UserReservationDAO.getInstance();
+        try {
+            List<UserReservationVO> urVOList = urDAO.selectUserReservation(userId);
+            if (urVOList.isEmpty()) {
+                result.append("false}");
+            } else {
+                result.append("true,");
+                result.append("\"result\":[");
+                for (int i = 0; i < urVOList.size(); i++) {
+                    result.append("{");
+                    result.append("\"reservationNumber\": \"" + urVOList.get(i).getReservationNumber() + "\",");
+                    result.append("\"reservationDate\": \"" + urVOList.get(i).getReservationDate() + "\",");
+                    result.append("\"movieTitle\": \"" + urVOList.get(i).getMovieTitle() + "\",");
+                    result.append("\"moviePosterPath\": \"" + urVOList.get(i).getMoviePosterPath() + "\",");
+                    result.append("\"theaterName\": \"" + urVOList.get(i).getTheaterName() + "\",");
+                    result.append("\"screeningDate\": \"" + urVOList.get(i).getScreeningDate() + "\",");
+                    result.append("\"seatLownumber\": \"" + urVOList.get(i).getSeatLownumber() + "\",");
+                    result.append("\"seatColnumber\": \"" + urVOList.get(i).getSeatColnumber() + "\",");
+                    result.append("\"totalPrice\": \"" + urVOList.get(i).getTotalPrice() + "\"");
+                    result.append("}");
+                    if (i < urVOList.size() - 1) {
+                        result.append(",");
+                    }
+                }
+                result.append("]}");
+            }
+        } catch (SQLException e) {
+            // 예외 처리
+            e.printStackTrace();
+            result.append("false}");
+        }
+        System.out.println(result);
+        return result.toString();
+    }
+}
