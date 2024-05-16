05007a5bfc62913d341fe5590d83ff334a8e50e4
iuiwiff <jimin5305@naver.com>
2024-05-16 12:37:54 +0900

    사용자_리뷰 진행중
    사용자_리뷰 ReviewDAO 수정
    사용자_리뷰 진행중

--- src/main/java/user/DAO/ReviewDAO.java
+++ src/main/java/user/DAO/ReviewDAO.java
@@ -136,4 +136,180 @@ public class ReviewDAO {
 		return list;
 	}//selectScoreReviewList
 	
+	
+	///////////////////////////////////리뷰 조회////////////////////////////////// 
+	public ReviewVO selectReview(String reservationNumber) throws SQLException {
+		ReviewVO rVO = new ReviewVO();
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		DbConnection dbCon = DbConnection.getInstance();
+		
+		try {
+			// 1. 데이터베이스 접속 정보
+			String id = "son";
+			String pass = "jimin";
+
+			// 2. 데이터베이스 연결
+			//3. Connection 얻기
+			con = dbCon.getConnection(id, pass);
+
+			//4. 쿼리문 생성 객체 얻기(Dynamic Query)
+			StringBuilder selectReview = new StringBuilder();
+			selectReview.append("	SELECT r.review_content, r.review_score, r.review_input_date,	")
+			.append("	m.movie_title, m.movie_poster_path	")
+			.append("	FROM review r	")
+			.append("	JOIN movie m ON r.movie_code = m.movie_code	");
+			
+			pstmt = con.prepareStatement(selectReview.toString());
+			
+			//6. 쿼리문 수행 후 결과 얻기
+			rs = pstmt.executeQuery();
+			
+			rVO = null;
+			while(rs.next()) {
+				ReviewVO rVOBuilder = rVO.builder()
+						.reviewContent(rs.getString("review_content"))
+						.reviewScore(rs.getInt("review_score"))
+						.reviewInputDate(rs.getString("review_input_date"))
+						.movieTitle(rs.getString("movie_title"))
+						.moviePosterPath(rs.getString("movie_poster_path"))
+						.build();
+				rVO = rVOBuilder;
+			}//end while
+			
+		}finally {
+			//7. 연결 끊기
+			dbCon.dbClose(rs, pstmt, con);
+		}//end finally
+		return rVO;
+	}//selectReview
+	
+	///////////////////////////////////리뷰 등록////////////////////////////////// 
+	public int insertReview(ReviewVO rVO) throws SQLException {
+		int cnt = 0;
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		DbConnection dbCon = DbConnection.getInstance();
+
+		try {
+			// 1. 데이터베이스 접속 정보
+			String id = "son";
+			String pass = "jimin";
+
+			// 2. 데이터베이스 연결
+			//3. Connection 얻기
+			con = dbCon.getConnection(id, pass);
+
+			//4. 쿼리문 생성 객체 얻기(Dynamic Query)
+			StringBuilder selectReview = new StringBuilder();
+			selectReview.append("	INSERT INTO review (review_number, reservation_number, review_content,	")
+					.append("	review_score, review_input_date, movie_code)	")
+					.append("	VALUES (review_sequence.nextval, ?, ?, ?, TO_CHAR(sysdate, 'YYYY-MM-DD'), ?");
+
+			pstmt = con.prepareStatement(selectReview.toString());
+
+			//5. 바인드 변수에 값 설정
+			pstmt.setString(1, rVO.getReservationNumber());
+			pstmt.setString(2, rVO.getReviewContent());
+			pstmt.setInt(3, rVO.getReviewScore());
+			pstmt.setString(4, rVO.getMovieCode());
+			
+			//6. 쿼리문 수행 후 결과 얻기
+			cnt = pstmt.executeUpdate();
+
+		} finally {
+			//7. 연결 끊기
+			dbCon.dbClose(rs, pstmt, con);
+		} // end finally
+		return cnt;
+	}// modifyReview
+	
+	
+	///////////////////////////////////리뷰 수정////////////////////////////////// 
+	public int modifyReview(ReviewVO rVO) throws SQLException {
+		int cnt = 0;
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		DbConnection dbCon = DbConnection.getInstance();
+
+		try {
+			// 1. 데이터베이스 접속 정보
+			String id = "son";
+			String pass = "jimin";
+
+			// 2. 데이터베이스 연결
+			//3. Connection 얻기
+			con = dbCon.getConnection(id, pass);
+
+			//4. 쿼리문 생성 객체 얻기(Dynamic Query)
+			StringBuilder selectReview = new StringBuilder();
+			selectReview.append("	UPDATE review	")
+					.append("	SET review_content = ?, review_score = ?,	")
+					.append("	review_input_date = TO_CHAR(sysdate, 'YYYY-MM-DD')	")
+					.append("	WHERE reservation_number = ?	");
+
+			pstmt = con.prepareStatement(selectReview.toString());
+
+			//5. 바인드 변수에 값 설정
+			pstmt.setString(1, rVO.getReviewContent());
+			pstmt.setInt(2, rVO.getReviewScore());
+			pstmt.setString(3, rVO.getReservationNumber());
+			
+			//6. 쿼리문 수행 후 결과 얻기
+			cnt = pstmt.executeUpdate();
+
+		} finally {
+			//7. 연결 끊기
+			dbCon.dbClose(rs, pstmt, con);
+		} // end finally
+		return cnt;
+	}// modifyReview
+	
+	
+	///////////////////////////////////리뷰 삭제////////////////////////////////// 
+	public int deleteReview(String reservationNumber) throws SQLException {
+		int cnt = 0;
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		DbConnection dbCon = DbConnection.getInstance();
+
+		try {
+			// 1. 데이터베이스 접속 정보
+			String id = "son";
+			String pass = "jimin";
+
+			// 2. 데이터베이스 연결
+			//3. Connection 얻기
+			con = dbCon.getConnection(id, pass);
+
+			//4. 쿼리문 생성 객체 얻기(Dynamic Query)
+			String deleteReview = "delete from review where reservation_number=?";
+
+			pstmt = con.prepareStatement(deleteReview);
+			
+			//5. 바인드 변수에 값 설정
+			pstmt.setString(1, reservationNumber);
+
+			//6. 쿼리문 수행 후 결과 얻기
+			cnt = pstmt.executeUpdate();
+
+		} finally {
+			//7. 연결 끊기
+			dbCon.dbClose(rs, pstmt, con);
+		} // end finally
+		return cnt;
+	}// deleteReview
+	
 }//class
